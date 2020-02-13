2b550236564c7cd6e0e899c513f2ec0e6b9d4536
Anna Melashkina <melashkina@outlook.com>
2020-02-13 15:55:41 +0100

    first commit

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainViewModel MainViewModel => DataContext as MainViewModel;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainViewModel.SelectRandomVideo();
+            Video.Stop();
+
+            // Workaround 1
+            //await Task.Delay(300);
+
+            Video.Play();
+        }
+
+        private void Video_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            var video = sender as MediaElement;
+            Debug.WriteLine($"Duration of video file: {video.NaturalDuration.TimeSpan.TotalMilliseconds} milliseconds");
+
+            // Workaround 2
+            // Video.Play();
+        }
+    }
+}
---
+++ App1/App1/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+
+namespace App1
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly List<string> _videos = new List<string>()
+        {
+            "ms-appx:///Assets/Videos/video1.mp4",
+            "ms-appx:///Assets/Videos/video2.mp4",
+            "ms-appx:///Assets/Videos/video3.mp4",
+        };
+
+        private string _videoPath;
+        public string VideoPath
+        {
+            get { return _videoPath; }
+            set { Set(ref _videoPath, value); }
+        }
+
+        internal void SelectRandomVideo()
+        {
+            var rnd = new Random();
+            VideoPath = _videos[rnd.Next(3)];
+        }
+    }
+}
